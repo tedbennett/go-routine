51dac3942392a81c12ecbb561ec66186daf77fb8
Ted Bennett <44608108+tedbennett@users.noreply.github.com>
2023-12-31 12:53:31 +0000

    Add json endpoints for goals + entries

--- cmd/goals/main.go
+++ cmd/goals/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"io"
 	"net/http"
 	"text/template"
+	"time"
 )
+
+// Templates
 
 type Template struct {
 	Templates *template.Template
@@ -31,6 +35,61 @@ func newTemplate(templates *template.Template) echo.Renderer {
 	}
 }
 
+// Models
+type Goal struct {
+	Id      uuid.UUID   `json:"id"`
+	Entries []GoalEntry `json:"entries"`
+	Title   string      `json:"title"`
+}
+
+type GoalEntry struct {
+	Date     time.Time `json:"date"`
+	Complete bool      `json:"complete"`
+}
+
+type Date struct {
+	year, day int
+	month     time.Month
+}
+
+func (d *Date) fromTimeDate(year int, month time.Month, day int) {
+	d.year, d.month, d.day = year, month, day
+}
+
+func (g *Goal) updateEntry() {
+	today := Date{}
+	today.year, today.month, today.day = time.Now().Date()
+	for i, entry := range g.Entries {
+		entryDate := Date{}
+		entryDate.fromTimeDate(entry.Date.Date())
+		if today == entryDate {
+			g.Entries[i].Complete = !entry.Complete
+			return
+		}
+	}
+	// No entry found with today's date
+	entry := GoalEntry{Date: time.Now(), Complete: true}
+	g.Entries = append(g.Entries, entry)
+}
+
+var goals []Goal
+
+func createGoal(title string) uuid.UUID {
+	id := uuid.New()
+	goal := Goal{Id: id, Entries: []GoalEntry{}, Title: title}
+	goals = append(goals, goal)
+	return id
+}
+
+func updateTodaysGoalEntry(id uuid.UUID) {
+	for i := range goals {
+		if goals[i].Id == id {
+			goals[i].updateEntry()
+			break
+		}
+	}
+}
+
 //go:generate npm run build
 func main() {
 	e := echo.New()
@@ -41,8 +100,44 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Static("static/css", "static/css")
 	NewTemplateRenderer(e, "static/*.html")
-	e.GET("/hello", func(c echo.Context) error {
+	e.GET("/", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "index", nil)
+	})
+
+	e.GET("/goals", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, goals)
+	})
+
+	e.POST("/goals", func(c echo.Context) error {
+		title := c.FormValue("title")
+		id := createGoal(title)
+		return c.JSON(http.StatusAccepted, id)
+	})
+
+	e.GET("/goals/:goalId", func(c echo.Context) error {
+		goalId := c.Param("goalId")
+		id, err := uuid.Parse(goalId)
+		if err != nil {
+			// Invalid uuid, won't exist
+			return c.NoContent(http.StatusNotFound)
+		}
+		for _, goal := range goals {
+			if goal.Id == id {
+				return c.JSON(http.StatusOK, goal)
+			}
+		}
+		return c.NoContent(http.StatusNotFound)
+	})
+
+	e.POST("/goals/:goalId/entries", func(c echo.Context) error {
+		goalId := c.Param("goalId")
+		id, err := uuid.Parse(goalId)
+		if err != nil {
+			// Invalid uuid, won't exist
+			return c.NoContent(http.StatusNotFound)
+		}
+		updateTodaysGoalEntry(id)
+		return c.NoContent(http.StatusAccepted)
 	})
 
 	e.Logger.Fatal(e.Start(":8000"))
