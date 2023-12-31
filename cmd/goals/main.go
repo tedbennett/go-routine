a007e830db42a8656d5bbdbdd4bd18d26d5f9e32
Ted Bennett <44608108+tedbennett@users.noreply.github.com>
2023-12-31 09:16:39 +0000

    Initial Commit

---
+++ cmd/goals/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"io"
+	"net/http"
+	"text/template"
+)
+
+type Template struct {
+	Templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.Templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplateRenderer(e *echo.Echo, paths ...string) {
+	tmpl := &template.Template{}
+	for i := range paths {
+		template.Must(tmpl.ParseGlob(paths[i]))
+	}
+	t := newTemplate(tmpl)
+	e.Renderer = t
+}
+
+func newTemplate(templates *template.Template) echo.Renderer {
+	return &Template{
+		Templates: templates,
+	}
+}
+func main() {
+	e := echo.New()
+
+	// Little bit of middlewares for housekeeping
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Use(middleware.Recover())
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
+
+	// This will initiate our template renderer
+	NewTemplateRenderer(e, "dist/*.html")
+	e.GET("/hello", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", nil)
+	})
+
+	e.Logger.Fatal(e.Start(":4040"))
+}
